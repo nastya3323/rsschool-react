50759311013e0e7d219291573397d33c20d7bd02
nastya3323 <gomes-96@mail.ru>
2026-05-04 15:55:00 +0400

    feat: teach the component to use state & change the value of the input field

--- rs-react-app/src/components/SearchBar/SearchBar.tsx
+++ rs-react-app/src/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,8 @@
 import Button from '../Button/Button';
 import './SearchBar.css';
-import { Component, type JSX } from 'react';
+import { Component, type ChangeEvent, type JSX } from 'react';
 
-const PLACEHOLDER__TEXT = 'Enter a character name, for example Luke';
+const PLACEHOLDER__TEXT = 'Enter a character name, for example Rick';
 
 const CLASSES = {
   SEARCH_BAR: 'search-bar',
@@ -10,11 +10,28 @@ const CLASSES = {
   SEARCH_BAR_BUTTON: 'search-bar__button',
 };
 
-export default class SearchBar extends Component {
+interface SearchBarProps {
+  searchQuery: string;
+  onSearchInput: (query: string) => void;
+}
+
+export default class SearchBar extends Component<SearchBarProps> {
+  private handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    this.props.onSearchInput(event.target.value);
+  };
+
   render(): JSX.Element {
+    const { searchQuery } = this.props;
+
     return (
       <div className={CLASSES.SEARCH_BAR}>
-        <input type="text" placeholder={PLACEHOLDER__TEXT} className={CLASSES.SEARCH_BAR_FIELD} />
+        <input
+          className={CLASSES.SEARCH_BAR_FIELD}
+          type="text"
+          placeholder={PLACEHOLDER__TEXT}
+          value={searchQuery}
+          onChange={this.handleInputChange}
+        />
         <Button className={CLASSES.SEARCH_BAR_BUTTON} onClick={() => console.log('Test')}>
           Find
         </Button>
